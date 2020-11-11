6fad2fc98bc994808ede346b8fada0c2668b6f6a
Jeremy jones <j1-aggie@hotmail.com>
2020-11-11 14:59:56 -0600

    saving

---
+++ static/js/bonus.js
@@ -0,0 +1,10 @@
+// Belly button biodiversity
+
+function getPlot(id) {
+    // pull data from the json file
+    d3.json('/data/samples.json').then((data) => {
+        console.log(data)
+    })
+
+
+}
